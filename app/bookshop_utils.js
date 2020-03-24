fb20b4148251ef8d2b20187766fddb8b276a85c7
Matt Mckinley <matt.g.mckinley@gmail.com>
2020-03-24 16:24:02 +0000

    completed questions

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -2,6 +2,36 @@ function addVAT(price) {
     return price + (0.2 * price);
 }
 
+function getFullName(firstName, lastName) {
+    return `${firstName} ${lastName}`;
+}
+
+function makeHalfPrice(price) {
+    return price / 2;
+}
+
+function countBooks(bookArray) {
+    return bookArray.length;
+}
+
+function isInStock(book) {
+    if (book.quantity > 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function getTotalOrderPrice(price, quantity) {
+    let total = price * quantity;
+    return total * 1.2;
+}
+
 module.exports = {
-    addVAT
+    addVAT,
+    getFullName,
+    makeHalfPrice,
+    countBooks,
+    isInStock,
+    getTotalOrderPrice
 };
--- test/utils.test.js
+++ test/utils.test.js
@@ -8,4 +8,60 @@ describe("utils.addVAT", () => {
   test("returns 120 when passed 100", () => {
     expect(utils.addVAT(100)).toBe(120);
   });
+
+  test("returns 118.8 when passed 99", () => {
+    expect(utils.addVAT(99)).toBe(118.8);
+  });
 });
+
+describe("utils.getFullName", () => {
+  test("returns Haruki Murakami when passed Haruki and Murakami", () => {
+    expect(utils.getFullName("Haruki", "Murakami")).toBe("Haruki Murakami");
+  });
+});
+
+describe("utils.makeHalfPrice", () => {
+  test("returns 5 when passed 10", () => {
+    expect(utils.makeHalfPrice(10)).toBe(5);
+  });
+});
+
+describe("utils.countBooks", () => {
+  test("returns total number of books in array", () => {
+    expect(utils.countBooks(["Book1", "Book2", "Book3", "Book4"])).toBe(4);
+  });
+});
+
+describe("utils.isInStock", () => {
+  test("returns true if the title is in stock", () => {
+    const book = {
+      title: "The Stone Diaries",
+      author: "Carol Shields",
+      yearOfPublication: 1993,
+      quantity: 9
+    };
+      expect(utils.isInStock(book)).toBe(true);
+  });
+});
+
+describe("utils.isInStock", () => {
+  test("returns false if the title is out of stock", () => {
+    const book = {
+      title: "The Stone Diaries",
+      author: "Carol Shields",
+      yearOfPublication: 1993,
+      quantity: 0
+    };
+      expect(utils.isInStock(book)).toBe(false);
+  });
+});
+
+describe("utils.getTotalOrderPrice", () => {
+  test("returns total price with VAT added", () => {
+    expect(utils.getTotalOrderPrice(8, 10)).toBe(96);
+  });
+});
+
+
+
+
